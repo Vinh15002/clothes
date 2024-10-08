c5179cac35eace334c29064370e51389c0fc07c6
= <vvvinhtran@gmail.com>
2024-10-08 23:51:07 +0700

    V

--- app.py
+++ app.py
@@ -15,13 +15,22 @@ def hello_world():
     
 @app.route('/', methods = ['POST'])
 def predict_image():
-    # imagelist = os.listdir('static/displayData')
-    # imagelist = ['displayData/' + image for image in imagelist]
-    # imagefile = request.files['imageperson']
+    imagelist = os.listdir('static/displayData')
+    imagelist = ['displayData/' + image for image in imagelist]
+    imagefile = request.files['imageperson']
     
-    # image_path = 'images/' + imagefile.filename
+    image_path = 'static/UploadImage/person.jpg'
     
-    # imagefile.save(image_path)
+    imagefile.save(image_path)
+    action = request.form['submit_action']
+    if(action == 'Gợi ý mẫu áo phù hợp'):
+        imagelist2 = os.listdir('static/recommentData/type0')
+        imagelist2 = ['recommentData/type0/' + image for image in imagelist2]
+        url = image_path
+        return  render_template('b.html', imageList = imagelist, imageList2 = imagelist2, image_url = url, DL = 'Flex')
+    else:
+        return "HELLO"
+    
     # l = request.form['imageclothes02']
     # if len(l)<1:
     #     imagefileclothe = request.files['imageclothes01']
@@ -36,42 +45,10 @@ def predict_image():
     # return render_template('b.html', imageList = imagelist)
     # return redirect(url_for('process'))
     return "HELLO"
-@app.route('/recomment', methods = ['GET'] )
-def DisplayRecomment():
-    imagelist = os.listdir('static/displayData')
-    imagelist = ['displayData/' + image for image in imagelist]
-    imagelist2 = os.listdir('static/recommentData/type1')
-    imagelist2 = ['recommentData/type1/' + image for image in imagelist2]
-    return render_template('b.html', imageList = imagelist, imageList2 = imagelist2)
 
 
-@app.route('/recomment', methods = ['POST'])
-def recomment():
-    imagelist = os.listdir('static/displayData')
-    imagelist = ['displayData/' + image for image in imagelist]
-    imagelist2 = os.listdir('static/recommentData/type1')
-    imagelist2 = ['recommentData/type1/' + image for image in imagelist2]
-    return redirect(url_for('DisplayRecomment'))
-    # imagefile = request.files['imageperson']
-    
-    # image_path = 'images/' + imagefile.filename
-    
-    # imagefile.save(image_path)
-    # l = request.form['imageclothes02']
-    # if len(l)<1:
-    #     imagefileclothe = request.files['imageclothes01']
-    #     image_path = 'images/clothes.jpg'
-    #     imagefileclothe.save(image_path)
-    # else:
-    #     imagefileclothe = request.form['imageclothes02']
-    #     imagefileclothe = imagefileclothe.replace('http://127.0.0.1:5000/','')
-    #     img = Image.open(imagefileclothe)
-    #     img.save('images/clothes.jpg')
 
-    # return render_template('b.html', imageList = imagelist)
-    # return redirect(url_for('process'))
-    # render_template('b.html', imageList = imagelist, imageList2=imagelist2)
-    
+
 
 
 @app.route('/process', methods = ['GET'])
